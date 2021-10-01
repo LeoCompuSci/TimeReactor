5c52731e85fc6ab01340ccc0185e09dd8d65b37b
Luan Luong <65574142+LeoCompuSci@users.noreply.github.com>
2021-10-02 00:05:47 +1000

    Time Reactor

---
+++ SimpleReactionMachine Tester/EnhancedReactionController.cs
@@ -0,0 +1,186 @@
+﻿using SimpleReactionMachine;
+using System;
+using System.Data;
+
+namespace SimpleReactionMachine
+{
+    public class EnhancedReactionController : IController
+    {
+        // Settings for the game times
+
+
+
+        const double TicksPerSecond = 100.0;
+        private const int EndingTime = 500;
+        private const int MaxReadyTime = 1000;
+
+
+        // Instance variables and properties
+        private State _state;
+        private IGui Gui { get; set; }
+        private IRandom Rng { get; set; }
+        private int Ticks { get; set; }
+        private int Times { get; set; }
+        private int TotalTime { get; set; }
+
+
+        public void Connect(IGui gui, IRandom rng)
+        {
+            Gui = gui;
+            Rng = rng;
+            Init();
+        }
+
+
+        public void Init() => _state = new Begin(this);
+
+
+        public void CoinInserted() => _state.CoinInserted();
+
+        public void GoStopPressed() => _state.GoStopPressed();
+
+
+        public void Tick() => _state.Tick();
+
+
+        void SetState(State state) => _state = state;
+
+
+        public abstract class State
+        {
+            protected EnhancedReactionController _controller;
+            public State(EnhancedReactionController controller) => _controller = controller;
+            public abstract void CoinInserted();
+            public abstract void GoStopPressed();
+            public abstract void Tick();
+        }
+
+
+        public class Begin : State
+        {
+            public Begin(EnhancedReactionController controller) : base(controller)
+            {
+                _controller.Times = 0;
+                _controller.TotalTime = 0;
+                _controller.Gui.SetDisplay("Insert coin");
+            }
+            public override void CoinInserted() => _controller.SetState(new Ready(_controller));
+            public override void GoStopPressed() { }
+            public override void Tick() { }
+        }
+
+
+        public class Ready : State
+        {
+            public Ready(EnhancedReactionController controller) : base(controller)
+            {
+                _controller.Gui.SetDisplay("Press Go!");
+                _controller.Ticks = 0;
+            }
+            public override void GoStopPressed()
+            {
+                _controller.SetState(new Wait(_controller));
+            }
+            public override void Tick()
+            {
+                _controller.Ticks++;
+                if (_controller.Ticks == MaxReadyTime)
+                    _controller.SetState(new Begin(_controller));
+            }
+            public override void CoinInserted() { }
+        }
+
+
+        public class Wait : State
+        {
+            private int _waitTime;
+            public Wait(EnhancedReactionController controller) : base(controller)
+            {
+                _controller.Gui.SetDisplay("Wait...");
+                _controller.Ticks = 0;
+                _waitTime = _controller.Rng.GetRandom(100, 250);
+            }
+            public override void GoStopPressed() => _controller.SetState(new Begin(_controller));
+            public override void Tick()
+            {
+                _controller.Ticks++;
+                if (_controller.Ticks == _waitTime)
+                {
+                    _controller.Times++;
+                    _controller.SetState(new Run(_controller));
+                }
+            }
+            public override void CoinInserted() { }
+        }
+
+        public class Run : State
+        {
+            public Run(EnhancedReactionController con) : base(con)
+            {
+                _controller.Gui.SetDisplay("0.00");
+                _controller.Ticks = 0;
+            }
+
+            public override void GoStopPressed()
+            {
+                _controller.TotalTime += _controller.Ticks;
+                _controller.SetState(new End(_controller));
+            }
+            public override void Tick()
+            {
+                _controller.Ticks++;
+                _controller.Gui.SetDisplay(
+                    (_controller.Ticks / TicksPerSecond).ToString("0.00"));
+                if (_controller.Ticks == 200)
+                    _controller.SetState(new End(_controller));
+            }
+            public override void CoinInserted() { }
+        }
+
+        public class End : State
+        {
+            public End(EnhancedReactionController con) : base(con)
+            {
+                _controller.Ticks = 0;
+            }
+            public override void GoStopPressed() => CheckTimes();
+            public override void Tick()
+            {
+                _controller.Ticks++;
+                if (_controller.Ticks == 300)
+                    CheckTimes();
+            }
+            private void CheckTimes()
+            {
+                if (_controller.Times == 3)
+                {
+                    _controller.SetState(new Result(_controller));
+                    return;
+                }
+                _controller.SetState(new Wait(_controller));
+            }
+            public override void CoinInserted() { }
+        }
+
+
+        public class Result : State
+        {
+            public Result(EnhancedReactionController con) : base(con)
+            {
+                _controller.Gui.SetDisplay("Average: "
+                    + (((double)_controller.TotalTime / _controller.Times) / TicksPerSecond)
+                    .ToString("0.00"));
+                _controller.Ticks = 0;
+            }
+            public override void Tick()
+            {
+                _controller.Ticks++;
+                if (_controller.Ticks == EndingTime)
+                    _controller.SetState(new Begin(_controller));
+            }
+            public override void GoStopPressed() => _controller.SetState(new Begin(_controller));
+            public override void CoinInserted() { }
+
+        }
+    }
+}
---
+++ SimpleReactionMachine Tester/Tester.cs
@@ -0,0 +1,204 @@
+﻿using System;
+
+namespace SimpleReactionMachine
+{
+    class Tester
+    {
+        private static IController controller;
+        private static IGui gui;
+        private static string displayText;
+        private static int randomNumber;
+        private static int passed = 0;
+
+        private class DummyGui : IGui
+        {
+
+            private IController controller;
+
+            public void Connect(IController controller)
+            {
+                this.controller = controller;
+            }
+
+            public void Init()
+            {
+                displayText = "?reset?";
+            }
+
+            public void SetDisplay(string msg)
+            {
+                displayText = msg;
+            }
+        }
+
+        private class RndGenerator : IRandom
+        {
+            public int GetRandom(int from, int to)
+            {
+                return randomNumber;
+            }
+        }
+
+        private static void Reset(string ch, IController controller, string msg)
+        {
+            try
+            {
+                controller.Init();
+                Message(ch, msg);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("test {0}: failed with exception {1})", ch, msg, exception.Message);
+            }
+        }
+
+        private static void GoStop(string ch, IController controller, string msg)
+        {
+            try
+            {
+                controller.GoStopPressed();
+                Message(ch, msg);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("test {0}: failed with exception {1})", ch, msg, exception.Message);
+            }
+        }
+
+        private static void InsertCoin(string ch, IController controller, string msg)
+        {
+            try
+            {
+                controller.CoinInserted();
+                Message(ch, msg);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("test {0}: failed with exception {1})", ch, msg, exception.Message);
+            }
+        }
+
+        private static void Ticks(string ch, IController controller, int n, string msg)
+        {
+            try
+            {
+                for (int t = 0; t < n; t++) controller.Tick();
+                Message(ch, msg);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("test {0}: failed with exception {1})", ch, msg, exception.Message);
+            }
+        }
+
+        private static void Message(string ch, string msg)
+        {
+            if (msg.ToLower() == displayText.ToLower())
+            {
+                Console.WriteLine("test {0}: passed successfully", ch);
+                passed++;
+            }
+            else
+                Console.WriteLine("test {0}: failed with message ( expected {1} | received {2})", ch, msg, displayText);
+        }
+
+
+        static void Main(string[] args)
+        {
+            // run simple test
+            EnhancedTest();
+            Console.WriteLine("\n=====================================\nSummary: {0} tests passed out of 40", passed);
+            Console.ReadKey();
+        }
+
+        private static void EnhancedTest()
+        {
+            //Construct a ReactionController
+            controller = new EnhancedReactionController();
+            gui = new DummyGui();
+
+            //Connect them to each other
+            gui.Connect(controller);
+            controller.Connect(gui, new RndGenerator());
+
+            //Reset the components()
+            gui.Init();
+
+            //Test the EnhancedReactionController
+            //THE COMPLETELY CIRCLE
+            Reset("1", controller, "Insert coin");
+            GoStop("2", controller, "Insert coin");
+            Ticks("3", controller, 1, "Insert coin");
+
+            InsertCoin("4", controller, "Press GO!");
+
+            Ticks("5", controller, 1, "Press GO!");
+            
+            Ticks("6", controller, 1001, "Insert coin");
+            InsertCoin("7", controller, "Press GO!");
+            InsertCoin("8", controller, "Press GO!");
+
+            randomNumber = 102;
+            GoStop("9", controller, "Wait...");
+            GoStop("10", controller, "Insert coin");
+            controller.CoinInserted();
+            controller.GoStopPressed();
+            //WAIT tick(s)
+            Ticks("11", controller, randomNumber - 1, "Wait...");
+
+            //RUN tick(s)
+            Ticks("12", controller, 11, "0.10");
+            Ticks("13", controller, 111, "1.21");
+
+            //goStop
+            GoStop("14", controller, "1.21");
+            InsertCoin("15", controller, "1.21");
+
+            GoStop("16", controller, "Wait...");
+            Ticks("17", controller, randomNumber - 1, "Wait...");
+            Ticks("18", controller, 23, "0.22");
+            Ticks("19", controller, 122, "1.44");
+            GoStop("20", controller, "1.44");
+
+
+            GoStop("21", controller, "Wait...");
+            Ticks("22", controller, randomNumber - 1, "Wait...");
+            Ticks("23", controller, 11, "0.10");
+            Ticks("24", controller, 90, "1.00");
+            GoStop("25", controller, "1.00");
+            //STOP tick(s)
+            Ticks("26", controller, 301, "Average: 1.22");
+            Ticks("27", controller, 501, "Insert Coin");
+
+            // IDLD >> READY >> TICK>10S >> IDLE???
+            InsertCoin("28", controller, "Press GO!");
+            Ticks("29", controller, 1001, "Insert Coin");
+
+
+            //IDLE >> READY >> WAIT >pressSTOP> IDLE???
+            controller.CoinInserted();
+            GoStop("30", controller, "Wait...");
+            GoStop("31", controller, "Insert coin");
+
+            //IDLE >> READY >> WAIT >> RUN???
+            gui.Init();
+            Reset("32", controller, "Insert coin");
+            controller.CoinInserted();
+            controller.GoStopPressed();
+            Ticks("33", controller, randomNumber - 1, "Wait...");
+
+            Ticks("34", controller, 9, "0.08");
+            Ticks("35", controller, 22, "0.30");
+
+            //IDLE -> READY -> WAIT -> RUN (timeout) -> STOP
+            gui.Init();
+            Reset("36", controller, "Insert coin");
+            randomNumber = 120;
+            InsertCoin("37", controller, "Press GO!");
+            GoStop("38", controller, "Wait...");
+            Ticks("39", controller, randomNumber + 199, "1.99");
+            Ticks("40", controller, 50, "2.00");
+        }
+    }
+
+}
